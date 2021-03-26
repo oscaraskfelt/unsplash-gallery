8b075897db081fd652017c320eb8435d935ba7de
Oscar Askfelt <oscaraskfelt@gmail.com>
2021-03-26 21:03:11 +0100

    add logic and styling to imgcontainer

--- src/components/ImageContainer/ImageContainer.jsx
+++ src/components/ImageContainer/ImageContainer.jsx
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image } from '../ImageComponent';
-import { Container } from './ImageContainer.style.js';
+import {
+  Container,
+  ControlSpan,
+  ArrowLeft,
+  ArrowRight,
+} from './ImageContainer.style.js';
+import { Modal } from '../Modal';
+
 const ImageContainer = ({ images }) => {
+  const [index, setindex] = useState(null);
+  const [showSplash, setShowSplash] = useState(false);
+
+  const enlarge = (index) => {
+    setindex(index);
+    setShowSplash(true);
+  };
+
+  const close = () => {
+    setShowSplash(false);
+  };
+
+  const nextImage = () => {
+    if (index + 1 < images.length) setindex((index) => index + 1);
+  };
+
+  const prevImage = () => {
+    if (index - 1 >= 0) setindex((index) => index - 1);
+  };
+
   return (
     <Container>
+      {showSplash && (
+        <Modal onClose={close}>
+          <Image
+            asBackground
+            src={images[index].urls.regular}
+            height={images[index].height}
+            width={images[index].width}
+          />
+          <ControlSpan>
+            <ArrowLeft onClick={prevImage} />
+            <ArrowRight onClick={nextImage} />
+          </ControlSpan>
+        </Modal>
+      )}
       {images &&
         images.map((item, i) => (
-          <Image key={i} src={item.urls.small} alt={item.alt_description} />
+          <Image
+            key={i}
+            src={item.urls.small}
+            alt={item.alt_description}
+            enlarge={() => enlarge(i)}
+          />
         ))}
     </Container>
   );
 };
 
 export default ImageContainer;
-
-{
-}
--- src/components/ImageContainer/ImageContainer.style.js
+++ src/components/ImageContainer/ImageContainer.style.js
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Breakpoints } from '../../styles/breakpoints';
+import { ArrowLeft as ArrLeft, ArrowRight as ArrRight } from 'react-feather';
 
 export const Container = styled.div`
   display: flex;
@@ -10,4 +11,27 @@ export const Container = styled.div`
     flex-flow: row wrap;
     justify-content: center;
   }
+`;
+
+export const ControlSpan = styled.span`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  color: white;
+`;
+
+const arrowStyle = css`
+  height: 3rem;
+  width: 3rem;
+  background: #33333366;
+  margin: 1rem;
+  border-radius: 50%;
+`;
+
+export const ArrowLeft = styled(ArrLeft)`
+  ${arrowStyle}
+`;
+export const ArrowRight = styled(ArrRight)`
+  ${arrowStyle}
 `;
